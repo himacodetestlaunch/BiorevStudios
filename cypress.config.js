c85f3863c0e0fd786c428a7af652fe5309e80019
himacodetestlaunch <himacodetestlaunch@gmail.com>
2025-06-13 12:06:46 -0700

    Updated cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,10 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://the-internet.herokuapp.com',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    chromeWebSecurity: false, // important! to disable web security for cross-origin requests
     defaultCommandTimeout: 5000,
     viewportWidth: 1280,
     viewportHeight: 720,
